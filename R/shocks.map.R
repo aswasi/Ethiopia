ec61d11ae37aa303f343ca1e58d6a55405f7adb2
tessam30 <tim.essam@gmail.com>
2015-07-01 00:02:13 -0400

    updates

--- R/shocks.map.R
+++ R/shocks.map.R
@@ -12,6 +12,7 @@ lapply(libs, require, character.only=T)
 
 # --- Get data, load it, jitter it and map it
 setwd("C:/Users/Tim/Documents/Ethiopia/Export/")
+setwd("C:/Users/t/Documents/Ethiopia/Export")
 d <- tbl_df(read.csv("LSMS.shocks.exploratory2014.csv"))
 
 # --- Filter on major shocks that are to be mapped
@@ -31,17 +32,56 @@ geo <- cbind(d.sub, jitgps)
 
 
 # Make map a little prettier and add in legend
-map <- leaflet(geo) %>% 
-  addTiles(group = "OSM (default)") %>%
-  addProviderTiles("CartoDB.Positron", group = "Carto-DB") %>%
-  addProviderTiles("Stamen.TonerLite", group = "Toner") %>%
-  addCircles(lat = ~lat, lng = ~ lon, color = ~priceShk, group = "Price") %>%
-    addLayersControl(
-    baseGroups = c("Base Map", "Carto-DB", "Toner"),
-    overlayGroups = c("Price"),
-    options = layersControlOptions(collapsed = FALSE)
-  ) 
-map
+# Create a palette that maps factor levels to colors
+
+geo$priceShk <- factor(geo$priceShk, levels = c(0, 1), 
+                      labels = c("No Shock", "Shock"))
+
+pal <- colorBin("Greens", domain = 0:1)
+
+basemap <- leaflet(geo) %>%   addProviderTiles("Acetate.terrain") %>%
+  addMarkers(clusterOptions = markerClusterOptions()) 
+basemap
+
+
+pal <- colorNumeric(palette = "Reds", domain = geo$totShocks)
+eth.map <- leaflet(geo) %>% addProviderTiles("Acetate.terrain") %>%  
+  addLegend(pal = pal, values = ~ totShocks, opacity = 1) %>%
+  addCircleMarkers(lat = ~ lat, lng = ~ lon, color = ~pal(totShocks),
+             radius = ~(geo$totShocks)*2, 
+             group = "Total", popup = ~ region,
+             fill = TRUE, fillColor = ~pal(totShocks), 
+             stroke = FALSE, fillOpacity = 0.5)  %>%
+  addMarkers(clusterOptions = markerClusterOptions()) 
+eth.map
+
+
+
+pal <- colorFactor(c("blue", "red"), domain = c("No Shock", "Shock"))
+pr.map <- leaflet(geo) %>% addProviderTiles("Acetate.terrain") %>%  
+  addLegend(pal = pal, values = ~ priceShk, opacity = 1) %>%
+  addCircleMarkers(lat = ~ lat, lng = ~ lon, color = ~pal(priceShk),
+                   radius =  ~ifelse(priceShk == "Shock", 7, 2),
+                   group = "Total", popup = ~ region,
+                   fill = TRUE, fillColor = ~pal(priceShk), 
+                   stroke = FALSE, fillOpacity = 0.5)
+pr.map
+
+
+geo$healthShk <- factor(geo$healthShk, levels = c(0, 1), 
+                       labels = c("No Shock", "Shock"))
+
+hlth.map <- leaflet(geo) %>% addProviderTiles("Acetate.terrain") %>%  
+  addLegend(pal = pal, values = ~ healthShk, opacity = 1) %>%
+  addCircleMarkers(lat = ~ lat, lng = ~ lon, color = ~pal(healthShk),
+                   radius =  ~ifelse(healthShk == "Shock", 7, 2),
+                   group = "Total", popup = ~ region,
+                   fill = TRUE, fillColor = ~pal(healthShk), 
+                   stroke = FALSE, fillOpacity = 0.5)
+hlth.map
+
+
+  
 
 
 
