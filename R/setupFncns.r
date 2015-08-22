49000790fc914991ae3f979beb4e95704c790cea
flaneuse <laura.d.hughes@gmail.com>
2015-08-21 20:30:42 -0500

    ! changed theme to eliminate the margins
    (b/w panel, plot, axes)
    
    necessary to decrease gap b/w edge of plot box and the plot.

--- R/setupFncns.r
+++ R/setupFncns.r
@@ -96,6 +96,11 @@ percent = function(x, ndigits = 1) {
   paste0(sprintf("%.f", round(x*100, ndigits)), "%")
 }
 
+
+# themes ------------------------------------------------------------------
+
+
+
 theme_laura <- function() {
   theme_bw() + 
     theme(axis.text = element_text(size = 14),
@@ -144,17 +149,44 @@ theme_blankbox <- function() {
 }
 
 theme_blankLH<- function() {
-  theme_classic() +
     theme(
+      title = element_blank(),
+      text = element_blank(),
+      line = element_blank(),
+      # rect = element_blank(),
+      plot.background = element_rect(colour = 'blue'),
+      panel.background = element_rect('dodgerblue'),
+      plot.margin = rep(unit(0, units = 'points'),4),
+      axis.ticks.margin = unit(0, units = 'points'),
+      legend.margin = rep(unit(0, units = 'points'),4),
+      panel.margin = rep(unit(0, units = 'points'),4),
       axis.text = element_blank(),
-      axis.title = element_blank(), 
       axis.line = element_blank(),
       axis.ticks = element_blank(),
+      axis.ticks.length = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.text.x = element_blank(),
       legend.position="none",
-      panel.grid.minor.x = element_blank(),
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.y = element_blank(),
-      panel.grid.major.y = element_blank())
+      panel.border = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank()
+      # panel.grid.minor.y = element_blank(),
+      # panel.grid.major.y = element_blank()
+      )
+}
+
+theme_new <- function() {
+  theme(
+  axis.title = element_blank(),
+  axis.text = element_blank(),
+  axis.ticks = element_blank(),
+  axis.ticks.length = unit(0, units = 'points'),
+  axis.ticks.margin = unit(0, units =  'points'),
+  panel.border = element_blank(),
+  plot.margin = rep(unit(0, units = 'points'),4)
+  )
 }
 
 theme_xOnly<- function() {
